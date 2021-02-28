4fbc94e411ae0454927b9c04f5aa9a5d669cdf84
ChuKangWang <ckwang19@gmail.com>
2021-02-28 20:54:19 +0800

    'record'

--- cc.py
+++ cc.py
@@ -14,18 +14,18 @@ def get_compound_growth(now_n, total_n, Cs):
 
 
 def main():
-    N = 4
+    N = 3
     total = 0
     cost = 0
     for i in range(N):
-        Cs = 4.3*8
+        Cs = 4.3*1
         cost += Cs/Gc
         principal_growth = get_principal_growth(i, Cs)
         compound_growth = get_compound_growth(i, N, Cs)
         sum_growth = principal_growth+compound_growth
         total += sum_growth
         invest_rate = pow(1+(total/cost), 1/i) if i != 0 else 0
-        print(f'in {i}st year, we can get: {round(principal_growth, 2)} + {round(compound_growth, 2)} = {round(sum_growth, 2)}, total={int(total)}, cost={round(cost, 2)}, invest_rate={round(invest_rate, 2)}')
+        print(f'the profit in {i}st year stock, we can get: {round(principal_growth, 2)} + {round(compound_growth, 2)} = {round(sum_growth, 2)}, total={round(total, 2)}, cost={round(cost, 2)}, invest_rate={round(invest_rate, 2)}')
 
 if __name__ == '__main__':
     main()
